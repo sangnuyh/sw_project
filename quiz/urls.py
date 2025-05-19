4f55979c5a090b98b0815b3ce4fa1ad815b7a97c
남상현 <goldnam5@dankook.ac.kr>
2025-05-19 19:54:10 +0900

    Revert "Merge pull request #2 from simjiseung/DB"
    This reverts commit 434f0835380a73e2f3b1b90f82d7a5a642575294, reversing
    changes made to 22f12f8de124825462db47cf497fd1b150fef20b.

--- quiz/urls.py
+++
@@ -1,9 +0,0 @@
-# quiz/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('save_result/', views.save_quiz_result, name='save_result'),
-    path('my_results/', views.my_results, name='my_results'),  # 추가
-]
